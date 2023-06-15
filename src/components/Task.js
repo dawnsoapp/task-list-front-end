467e942a5531ed7474ca9bb1b2d811234c183fdf
IzzyBravo <Izzybravo41@gmail.com>
2023-06-15 11:58:30 -0700

    idk

--- src/components/Task.js
+++ src/components/Task.js
@@ -20,3 +20,36 @@ Task.propTypes = {
 };
 
 export default Task;
+
+// 1.What props does Task have? Where do they come from?
+// id, title, isComplete.
+// comes from parent const TASKS 
+
+// How would the code change if {id, title, isComplete} were replaced with props
+// doesn't matter
+// const SayHello = (props) => {
+//   return <p>{props.greeting}, {props.name}!</p>
+// };
+
+
+
+// // What props does TaskList have? Where do they come from?
+// TaskList has tasks as a props and comes from Task.js
+
+
+// // Where is the function getTaskListJSX called in TaskList?
+// // In componenet tasklist
+
+// // How would the code change without this helper function?
+// wouldn't be able to be defined
+// wouldn't be able to make a list to accees index item
+// we wouldnt be able to use the array map?
+
+// What component is TASKS passed to in App?
+// being called within TaskList
+
+// How does the component pass TASKS?
+// tasklist passes tasks to make individual tasks the app wants and formats it to make it nice
+
+// What element is the component wrapped in?
+{/* <div> element</div> */}
