e1f645dc65399ad3b5a1f24cdeebba47b5593ae4
IzzyBravo <Izzybravo41@gmail.com>
2023-06-16 14:25:55 -0700

    updating troublshoot

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,20 +5,34 @@ import './Task.css';
 
 const Task = (props) => {
   // const [complete, setComplete] = useState(isComplete);
-  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
+  // const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
 
-  return (
-    <li className="tasks__item">
-      <button
-        className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => props.onTaskUpdate(props.id)}
-      >
-        {props.title}
-      </button>
-      <button className="tasks__item__remove button">x</button>
-    </li>
-  );
-};
+//   return (
+//     <li className="tasks__item">
+//       <button
+//         className={`tasks__item__toggle ${buttonClass}`}
+//         onClick={() => props.onTaskUpdate(props.id)}
+//       >
+//         {props.title}
+//       </button>
+//       <button className="tasks__item__remove button">x</button>
+//     </li>
+//   );
+    return (
+      <li className="tasks__item">
+        <button
+          className={`tasks__item__toggle `}
+          onClick={() => props.onTaskUpdate(props.id)}
+        >
+          {props.title}
+        </button>
+        <button className="tasks__item__remove button">x</button>
+      </li>
+    );
+
+  };
+
+
 
 Task.propTypes = {
   id: PropTypes.number.isRequired,
